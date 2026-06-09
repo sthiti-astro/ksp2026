c8e28fc614de4554fdff0dafc5c7a4dc7e082cfb
anirudhsalgundi <134363781+anirudhsalgundi@users.noreply.github.com>
2026-06-09 08:44:42 -0400

    skeleton py file for crossmatches

---
+++ crossmatches.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def load_swift_data():
+    return None
+
+def crossmatch_ztf():
+    return None
+
+def crossmatch_atlas():
+    return None
+
+def make_summary():
+    return None
+
+
+def main():
+    """
+    """
+    return None
+
+if __name__ == "__main__":
+    main()
